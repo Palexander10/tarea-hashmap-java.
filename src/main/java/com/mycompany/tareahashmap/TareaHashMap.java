bac43126600ade8339f427be8c3c9c3291937a04
Palexander10 <pa.ramonm@uea.edu.ec>
2025-09-25 17:37:38 -0500

    Creando mapa

---
+++ src/main/java/com/mycompany/tareahashmap/TareaHashMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareahashmap;
+
+/**
+ *
+ * @author Pablo Alexander
+ */
+public class TareaHashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
